d34d00ecefb7fc804e1e2e3299f1a880fe42134b
Programador 02 <desenvolvedor2@f5promotora.com.br>
2019-02-15 17:36:10 -0300

    2

--- src/app/modules/reactive-forms/reactive-forms.component.ts
+++ src/app/modules/reactive-forms/reactive-forms.component.ts
@@ -1,52 +1,85 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, FormGroupDirective, NgForm, FormGroup, FormArray } from '@angular/forms';
 import { forbiddenNameValidator } from 'src/app/shared/user-name.validator';
 import { passwordValidator } from 'src/app/shared/password.validator';
 import { ErrorStateMatcher } from '@angular/material';
+import { RegistrationService } from "../../services/registration.service";
 
 /** Error when the parent is invalid */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const invalidCtrl = !!(control && control.invalid && control.parent.dirty) && (control.touched);
-    const invalidParent = !!(control && control.parent && control.parent.hasError('misMatch') && control.parent.dirty);
+	isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+		const invalidCtrl = !!(control && control.invalid && control.parent.dirty) && (control.touched);
+		const invalidParent = !!(control && control.parent && control.parent.hasError('misMatch') && control.parent.dirty);
 
-    return (invalidCtrl || invalidParent) || (control.untouched && form.submitted) || (control.touched && control.hasError('required'))
-  }
+		return (invalidCtrl || invalidParent) || (control.untouched && form.submitted) || (control.touched && control.hasError('required'))
+	}
 }
 
 @Component({
-  selector: 'app-reactive-forms',
-  templateUrl: './reactive-forms.component.html',
-  styleUrls: ['./reactive-forms.component.scss']
+	selector: 'app-reactive-forms',
+	templateUrl: './reactive-forms.component.html',
+	styleUrls: ['./reactive-forms.component.scss']
 })
 export class ReactiveFormsComponent implements OnInit {
 
-  errorMatcher = new MyErrorStateMatcher()
+	registrationForm: FormGroup
 
-  registrationForm = this.formBuilder.group({
-    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/), forbiddenNameValidator(/admin/)]],
-    password: ['', [Validators.required]],
-    confirmPassword: ['', [Validators.required]],
-    address: this.formBuilder.group({
-      city: [''],
-      state: [''],
-      postalCode: ['']
-    })
-  }, {validator: passwordValidator})
+	errorMatcher = new MyErrorStateMatcher()
 
-  constructor(private formBuilder: FormBuilder) { }
+	get alternateEmails() {
+		return this.registrationForm.get('alternateEmails') as FormArray
+	}
 
-  ngOnInit() {
-  }
+	constructor(private formBuilder: FormBuilder, private _registrationService: RegistrationService) { }
 
-  loadApiData() {
-    this.registrationForm.patchValue({
-      userName: 'Cleudo',
-      address: {
-        city: 'Xoró Limão',
-        state: 'Ceará'
-      }
-    })
-  }
+	ngOnInit() {
+
+		this.registrationForm = this.formBuilder.group({
+			userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/), forbiddenNameValidator(/admin/)]],
+			email: [''],
+			subscribe: [false],
+			password: ['', [Validators.required]],
+			confirmPassword: ['', [Validators.required]],
+			address: this.formBuilder.group({
+				city: [''],
+				state: [''],
+				postalCode: ['']
+			}),
+			alternateEmails: this.formBuilder.array([])
+		}, {validator: passwordValidator})
+
+		this.registrationForm.get('subscribe').valueChanges
+			.subscribe(checkedValue => {
+				const email = this.registrationForm.get('email')
+				if (checkedValue) {
+					email.setValidators(Validators.required)
+				} else {
+					email.clearValidators()
+				}
+				email.updateValueAndValidity()
+			})
+	}
+
+	addAlternateEmail() {
+		this.alternateEmails.push(this.formBuilder.control(''))
+	}
+
+	loadApiData() {
+		this.registrationForm.patchValue({
+			userName: 'Cleudo',
+			address: {
+				city: 'Xoró Limão',
+				state: 'Ceará'
+			}
+		})
+	}
+	
+	onSubmit() {
+		this._registrationService.register(this.registrationForm.value)
+			.subscribe(
+				response => console.log('Success', response),
+				error => console.log('Error', error)
+			)
+	}
 
 }
